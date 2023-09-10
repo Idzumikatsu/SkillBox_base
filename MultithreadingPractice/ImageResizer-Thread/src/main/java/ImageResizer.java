74dabc7ac87c40de97d19456008a223ce06c1ff2
k0na <pwpvpdru@yandex.ru>
2023-09-10 18:00:07 +0300

    - cleanup repo

--- MultithreadingPractice/ImageResizer-Thread/src/main/java/ImageResizer.java
+++ MultithreadingPractice/ImageResizer-Thread/src/main/java/ImageResizer.java
@@ -1,15 +1,18 @@
+import org.imgscalr.Scalr;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.List;
 
-public class ImageResizer implements Runnable{
+public class ImageResizer extends Thread {
 
-    private File[] files;
+    private List<File> files;
     private int newWidth;
     private String dstFolder;
     private long start;
 
-    public ImageResizer(File[] files, int newWidth, String dstFolder, long start) {
+    public ImageResizer(List<File> files, int newWidth, String dstFolder, long start) {
         this.files = files;
         this.newWidth = newWidth;
         this.dstFolder = dstFolder;
@@ -26,25 +29,20 @@ public class ImageResizer implements Runnable{
                     continue;
                 }
 
-                int newHeight = (int) Math.round(
-                        image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                        newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
+                BufferedImage newImage =
+                        Scalr.resize(
+                                image,
+                                Scalr.Method.ULTRA_QUALITY,
+                                Scalr.Mode.FIT_TO_WIDTH,
+                                newWidth,
+                                Scalr.OP_ANTIALIAS
+                        );
 
                 File newFile = new File(dstFolder + "/" + file.getName());
                 ImageIO.write(newImage, "jpg", newFile);
+
+                image.flush();
+                newImage.flush();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
