d682405c70b102690bb436597795725a470a8e92
k0na <pwpvpdru@yandex.ru>
2023-09-23 22:28:34 +0300

    lesson_20.18 homework - optimize main method - add multithreading with ThreadPool

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,20 +1,40 @@
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Loader {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
-        PrintWriter writer = new PrintWriter("res/numbers.txt");
+        try {
+            char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+            List<Thread> threads = new ArrayList<>();
+            try (ExecutorService service = Executors.newCachedThreadPool()) {
+                for (int regionCode = 1; regionCode < 100; regionCode++) {
+                    PrintWriter writer = new PrintWriter("res/" + regionCode + ".txt");
+                    Thread t = getThread(regionCode, letters, writer);
+                    service.execute(t);
+                    threads.add(t);
+                }
+                for (Thread thread : threads) {
+                    thread.join();
+                }
+            }
+        } catch (FileNotFoundException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println((System.currentTimeMillis() - start) + " ms");
+    }
 
-        char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-
-        for (int regionCode = 0; regionCode < 100; regionCode++) {
-            StringBuilder sb = new StringBuilder();
+    private static Thread getThread(int regionCode, char[] letters, PrintWriter writer) {
+        return new Thread(() -> {
             for (int number = 1; number < 1000; number++) {
+                StringBuilder sb = new StringBuilder();
                 for (char firstLetter : letters) {
                     for (char secondLetter : letters) {
                         for (char thirdLetter : letters) {
@@ -27,13 +47,10 @@ public class Loader {
                         }
                     }
                 }
+                writer.write(sb.toString());
+                writer.flush();
             }
-            writer.write(sb.toString());
-        }
-        writer.flush();
-        writer.close();
-
-        System.out.println((System.currentTimeMillis() - start) + " ms");
+        });
     }
 
     private static String padNumber(int number, int numberLength) {
