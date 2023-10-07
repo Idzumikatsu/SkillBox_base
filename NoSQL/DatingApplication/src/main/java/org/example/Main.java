4097574eecb76a84bc4dded77775f16e313f1924
k0na <pwpvpdru@yandex.ru>
2023-10-07 14:00:36 +0300

    lesson_23.5 homework is done - realize infinity Redis loop with paid interrupts

--- NoSQL/DatingApplication/src/main/java/org/example/Main.java
+++ NoSQL/DatingApplication/src/main/java/org/example/Main.java
@@ -1,52 +1,39 @@
 package org.example;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
-
-import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Main {
 
-    // Запуск докер-контейнера:
-    // docker run --rm --name skill-redis -p 127.0.0.1:6379:6379/tcp -d redis
-
-    // Для теста будем считать неактивными пользователей, которые не заходили 2 секунды
-    private static final int DELETE_SECONDS_AGO = 2;
-
-    // Допустим пользователи делают 500 запросов к сайту в секунду
-    private static final int RPS = 500;
-
-    // И всего на сайт заходило 1000 различных пользователей
-    private static final int USERS = 1000;
-
-    // Также мы добавим задержку между посещениями
-    private static final int SLEEP = 1; // 1 миллисекунда
-
-    private static final SimpleDateFormat DF = new SimpleDateFormat("HH:mm:ss");
-
-    private static void log(int UsersOnline) {
-        String log = String.format("[%s] Пользователей онлайн: %d", DF.format(new Date()), UsersOnline);
-        out.println(log);
-    }
+    private static final int USERS_COUNT = 20;
+    private static final int PAID_SLEEP = 1000;
 
     public static void main(String[] args) throws InterruptedException {
 
         RedisStorage redis = new RedisStorage();
         redis.init();
-        // Эмулируем 10 секунд работы сайта
-        for(int seconds=0; seconds <= 10; seconds++) {
-            // Выполним 500 запросов
-            for(int request = 0; request <= RPS; request++) {
-                int user_id = new Random().nextInt(USERS);
-                redis.logPageVisit(user_id);
-                Thread.sleep(SLEEP);
+
+        while (true){
+            redis.addUsers(USERS_COUNT);
+            List<String> paidUsers = new ArrayList<>();
+
+            for (String user : redis.getAllUsers()) {
+
+                if (redis.isPaidUserCheck()){
+                    String paidUser = String.valueOf(
+                            ThreadLocalRandom.current().nextInt(1, USERS_COUNT + 1));
+                    paidUsers.add(paidUser);
+                    redis.printPaidInfo(paidUser);
+                    Thread.sleep(PAID_SLEEP);
+                }
+
+                if (!paidUsers.contains(user)){
+                    redis.printRegularInfo(user);
+                    redis.getAllUsers().remove(user);
+                }
             }
-            redis.deleteOldEntries(DELETE_SECONDS_AGO);
-            int usersOnline = redis.calculateUsersNumber();
-            log(usersOnline);
         }
-        redis.shutdown();
     }
 }
--- NoSQL/DatingApplication/src/main/java/org/example/RedisStorage.java
+++ NoSQL/DatingApplication/src/main/java/org/example/RedisStorage.java
@@ -8,69 +8,50 @@ import org.redisson.client.RedisConnectionException;
 import org.redisson.config.Config;
 
 import java.util.Date;
-import static java.lang.System.out;
 
 public class RedisStorage {
 
-    // Объект для работы с Redis
     private RedissonClient redisson;
-
-    // Объект для работы с ключами
-    private RKeys rKeys;
-
-    // Объект для работы с Sorted Set'ом
-    private RScoredSortedSet<String> onlineUsers;
-
-    private final static String KEY = "ONLINE_USERS";
+    private RScoredSortedSet<String> users;
+    private final static String KEY = "NEW_USERS";
 
     private double getTs() {
-        return new Date().getTime() / 1000;
+        return new Date().getTime();
     }
 
-    // Пример вывода всех ключей
-    public void listKeys() {
-        Iterable<String> keys = rKeys.getKeys();
-        for(String key: keys) {
-            out.println("KEY: " + key + ", type:" + rKeys.getType(key));
-        }
-    }
-
-    void init() {
+    public void init() {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         try {
             redisson = Redisson.create(config);
         } catch (RedisConnectionException Exc) {
-            out.println("Не удалось подключиться к Redis");
-            out.println(Exc.getMessage());
+            System.out.println("Не удалось подключиться к Redis");
+            System.out.println(Exc.getMessage());
         }
-        rKeys = redisson.getKeys();
-        onlineUsers = redisson.getScoredSortedSet(KEY);
+        RKeys rKeys = redisson.getKeys();
+        users = redisson.getScoredSortedSet(KEY);
         rKeys.delete(KEY);
     }
 
-    void shutdown() {
-        redisson.shutdown();
+    public void addUsers(int usersCount) {
+        for (int i = 1; i <= usersCount; i++) {
+            users.add(getTs(), String.valueOf(i));
+        }
     }
 
-    // Фиксирует посещение пользователем страницы
-    void logPageVisit(int user_id)
-    {
-        //ZADD ONLINE_USERS
-        onlineUsers.add(getTs(), String.valueOf(user_id));
+    public RScoredSortedSet<String> getAllUsers() {
+        return users;
     }
 
-    // Удаляет
-    void deleteOldEntries(int secondsAgo)
-    {
-        //ZREVRANGEBYSCORE ONLINE_USERS 0 <time_5_seconds_ago>
-        onlineUsers.removeRangeByScore(0, true, getTs() - secondsAgo, true);
-
-
+    public boolean isPaidUserCheck() {
+        return Math.random() < 0.1;
     }
-    int calculateUsersNumber()
-    {
-        //ZCOUNT ONLINE_USERS
-        return onlineUsers.count(Double.NEGATIVE_INFINITY, true, Double.POSITIVE_INFINITY, true);
+
+    public void printRegularInfo(String user){
+        System.out.println("На главной странице показываем пользователя " + user);
+    }
+
+    public void printPaidInfo(String user){
+        System.out.println("Пользователь " + user + " оплатил платную услугу\nНа главной странице 1 сек платно показываем пользователя " + user);
     }
 }
